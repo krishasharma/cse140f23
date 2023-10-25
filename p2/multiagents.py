086c31a30c369bf4c36c54a41653be540e95ad2f
Krisha Sharma <krishasharma@krishasmacbookpro.local>
2023-10-25 15:11:08 -0700

    p2 started- multiagents.py added, reflexAgent started

---
+++ p2/multiagents.py
@@ -0,0 +1,182 @@
+import random
+
+from pacai.agents.base import BaseAgent
+from pacai.agents.search.multiagent import MultiAgentSearchAgent
+from pacai.core.distance import manhattan
+
+class ReflexAgent(BaseAgent):
+    """
+    A reflex agent chooses an action at each choice point by examining
+    its alternatives via a state evaluation function.
+
+    The code below is provided as a guide.
+    You are welcome to change it in any way you see fit,
+    so long as you don't touch the method headers.
+    """
+
+    def __init__(self, index, **kwargs):
+        super().__init__(index, **kwargs)
+
+    def getAction(self, gameState):
+        """
+        You do not need to change this method, but you're welcome to.
+
+        `ReflexAgent.getAction` chooses among the best options according to the evaluation function.
+
+        Just like in the previous project, this method takes a
+        `pacai.core.gamestate.AbstractGameState` and returns some value from
+        `pacai.core.directions.Directions`.
+        """
+
+        # Collect legal moves.
+        legalMoves = gameState.getLegalActions()
+
+        # Choose one of the best actions.
+        scores = [self.evaluationFunction(gameState, action) for action in legalMoves]
+        bestScore = max(scores)
+        bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
+        chosenIndex = random.choice(bestIndices)  # Pick randomly among the best.
+
+        return legalMoves[chosenIndex]
+        # get action needs to return the best action as pacman
+
+    def evaluationFunction(self, currentGameState, action):
+        """
+        Design a better evaluation function here.
+
+        The evaluation function takes in the current `pacai.bin.pacman.PacmanGameState`
+        and an action, and returns a number, where higher numbers are better.
+        Make sure to understand the range of different values before you combine them
+        in your evaluation function.
+        """
+
+        # Useful information you can extract.
+        # newPosition = successorGameState.getPacmanPosition()
+        # oldFood = currentGameState.getFood()
+        # newGhostStates = successorGameState.getGhostStates()
+        # newScaredTimes = [ghostState.getScaredTimer() for ghostState in newGhostStates]
+
+        # *** Your Code Here ***
+        # generate the successor game state
+        # after taking in the action that was specificed
+        successorGameState = currentGameState.generatePacmanSuccessor(action)
+        newPos = successorGameState.getPacmanPosition()
+        newFood = successorGameState.getFood()
+        newGhostStates = successorGameState.getGhostStates()
+        newScaredTimes = [ghostState.getScaredTimer() for ghostState in newGhostStates]
+
+        # initialize a score based on the current game state's score
+        score = successorGameState.getScore()
+
+        # calculate distances to the closest food pellet
+        foodDistances = [manhattan(newPos, food) for food in newFood.asList()]
+        if foodDistances:
+            closestFoodDistance = min(foodDistances)
+            # add a positive score for being closer to the food
+            # the reciprocal of the distance is used here???
+            score += 1.0 / closestFoodDistance
+
+        # avoid ghosts if they are not scared, and approach them if they are
+        for ghost, scaredTime in zip(newGhostStates, newScaredTimes):
+            ghostPos = ghost.getPosition()
+            # if a ghost is too close, and not scared
+            # then aviod it (negative score?)
+            if scaredTime == 0 and manhattan(newPos, ghostPos) < 2:
+                score -= 1000  # strongly avoid ghosts
+            # if a ghost is too close and scared
+            # approach it (positive score)
+            elif scaredTime > 0 and manhattan(newPos, ghostPos) < 2:
+                score += 500  # approach scared ghosts
+
+        return score
+
+        return successorGameState.getScore()
+
+class MinimaxAgent(MultiAgentSearchAgent):
+    """
+    A minimax agent.
+
+    Here are some method calls that might be useful when implementing minimax.
+
+    `pacai.core.gamestate.AbstractGameState.getNumAgents()`:
+    Get the total number of agents in the game
+
+    `pacai.core.gamestate.AbstractGameState.getLegalActions`:
+    Returns a list of legal actions for an agent.
+    Pacman is always at index 0, and ghosts are >= 1.
+
+    `pacai.core.gamestate.AbstractGameState.generateSuccessor`:
+    Get the successor game state after an agent takes an action.
+
+    `pacai.core.directions.Directions.STOP`:
+    The stop direction, which is always legal, but you may not want to include in your search.
+
+    Method to Implement:
+
+    `pacai.agents.base.BaseAgent.getAction`:
+    Returns the minimax action from the current gameState using
+    `pacai.agents.search.multiagent.MultiAgentSearchAgent.getTreeDepth`
+    and `pacai.agents.search.multiagent.MultiAgentSearchAgent.getEvaluationFunction`.
+    """
+
+    def __init__(self, index, **kwargs):
+        super().__init__(index, **kwargs)
+
+class AlphaBetaAgent(MultiAgentSearchAgent):
+    """
+    A minimax agent with alpha-beta pruning.
+
+    Method to Implement:
+
+    `pacai.agents.base.BaseAgent.getAction`:
+    Returns the minimax action from the current gameState using
+    `pacai.agents.search.multiagent.MultiAgentSearchAgent.getTreeDepth`
+    and `pacai.agents.search.multiagent.MultiAgentSearchAgent.getEvaluationFunction`.
+    """
+
+    def __init__(self, index, **kwargs):
+        super().__init__(index, **kwargs)
+
+class ExpectimaxAgent(MultiAgentSearchAgent):
+    """
+    An expectimax agent.
+
+    All ghosts should be modeled as choosing uniformly at random from their legal moves.
+
+    Method to Implement:
+
+    `pacai.agents.base.BaseAgent.getAction`:
+    Returns the expectimax action from the current gameState using
+    `pacai.agents.search.multiagent.MultiAgentSearchAgent.getTreeDepth`
+    and `pacai.agents.search.multiagent.MultiAgentSearchAgent.getEvaluationFunction`.
+    """
+
+    def __init__(self, index, **kwargs):
+        super().__init__(index, **kwargs)
+
+def betterEvaluationFunction(currentGameState):
+    """
+    Your extreme ghost-hunting, pellet-nabbing, food-gobbling, unstoppable evaluation function.
+
+    DESCRIPTION: <write something here so we know what you did>
+    """
+
+    return currentGameState.getScore()
+
+class ContestAgent(MultiAgentSearchAgent):
+    """
+    Your agent for the mini-contest.
+
+    You can use any method you want and search to any depth you want.
+    Just remember that the mini-contest is timed, so you have to trade off speed and computation.
+
+    Ghosts don't behave randomly anymore, but they aren't perfect either -- they'll usually
+    just make a beeline straight towards Pacman (or away if they're scared!)
+
+    Method to Implement:
+
+    `pacai.agents.base.BaseAgent.getAction`
+    """
+
+    def __init__(self, index, **kwargs):
+        super().__init__(index, **kwargs)
